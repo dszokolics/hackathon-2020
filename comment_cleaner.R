ea7abd9a850432d83defefa2eef7461000fd45bc
molnar95 <molnar.bence95@gmail.com>
2020-09-28 09:04:00 +0200

    add some changes

--- comment_cleaner.R
+++ comment_cleaner.R
@@ -7,7 +7,6 @@ library(ggplot2)
 library(stringr)
 library(textdata)
 library(topicmodels)
-#install.packages('lubridate')
 library(lubridate)
 
 
@@ -15,9 +14,11 @@ rm(list = ls())
 memory.limit(size=56000)
 
 
+###############
+# Read Data   #
+###############
+
 setwd('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/collected')
-
-
 
 comments_all <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/collected/items_185_245.RDS')
 str(comments_all)
@@ -26,11 +27,16 @@ str(comments_all)
 abt <- read.csv("G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/ABT_base_full.csv", sep=',', header=T)
 str(abt)
 
-
+# select comments based on ABT's ID-s
 comments_recent <- comments_all[comments_all$by %in% unique(abt$by), ]
-
-
 comments_recent <- comments_all[comments_all$type == "comment", ]
+
+###################
+# Data Cleanings  #
+###################
+
+
+# create date field
 comments_recent$time <- as.Date(as.POSIXct(comments_recent$time, origin="1970-01-01"))
 comments_recent$time <- floor_date(as_date(comments_recent$time), "month")
 
@@ -49,11 +55,6 @@ comments_recent$text <- gsub("&#x27;", "'", comments_recent$text)
 comments_recent$cnt_links <- str_count(comments_recent$text, pattern = "<a.*?/a>")
 comments_recent$text <- gsub("<a.*?/a>", "", comments_recent$text)
 
-str(comments_recent)
-
-saveRDS(comments_recent, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_processed.rds')
-
-comments_recent <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_processed.rds')
 
 # regex HTML tags like <p></p>
 comments_recent$text <- gsub("<.*?>", "", comments_recent$text)
@@ -63,10 +64,6 @@ comments_recent$text <- gsub("\n", "'", comments_recent$text)
 comments_recent$cnt_qout <- str_count(comments_recent$text, pattern = "&quot;")
 comments_recent$text <- gsub("&quot;", "", comments_recent$text)
 
-str(comments_recent)
-
-saveRDS(comments_recent, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_processed.rds')
-comments_recent <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_processed.rds')
 
 comments_recent$text <- gsub("nâ\200\2", "e", comments_recent$text) 
 
@@ -78,17 +75,12 @@ comments_recent$text <- gsub("&#x2F;", " ", comments_recent$text)
 comments_recent$cnt_exclam <- str_count(comments_recent$text, pattern = "!")
 comments_recent$cnt_ques <- str_count(comments_recent$text, pattern = '\\?')
 
-str(comments_recent)
-saveRDS(comments_recent, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_processed.rds')
-comments_recent <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_processed.rds')
-
 
 # lowercasing the text:
 comments_recent$text <- tolower(comments_recent$text)
 
 
 # drop non alphanumerical characters
-
 comments_recent$text <- gsub("'.*? ", " ", comments_recent$text)
 comments_recent$text <- gsub("[^[:alnum:] ]", " ", comments_recent$text)
 
@@ -99,27 +91,6 @@ comments_recent$text <- gsub("[0-9]", " ", comments_recent$text)
 comments_recent <- comments_recent[comments_recent$text != "", ]
 
 comments_recent$text <- gsub("â€™", "'", comments_recent$text)
-
-
-saveRDS(comments_recent, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_processed2.rds')
-comments_recent <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_processed2.rds')
-
-str(comments_recent)
-
-
-comments_recent1 <- comments_recent[1:1000000, ]
-comments_recent2 <- comments_recent[1000001:2000000, ]
-comments_recent3 <- comments_recent[2000001:2824962, ]
-
-saveRDS(comments_recent1, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent1.rds')
-saveRDS(comments_recent2, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent2.rds')
-saveRDS(comments_recent3, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent3.rds')
-
-
-comments_recent1 <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent1.rds')
-comments_recent2 <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent2.rds')
-comments_recent3 <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent3.rds')
-
 
 
 # drop character after '
@@ -135,9 +106,7 @@ comments_recent$text <- gsub("'.*? ", " ", comments_recent$text)
 
 # tokenization
 # stopword removal
-
-
-comments_tokenized <- comments_recent3  %>%
+comments_tokenized <- comments_recent  %>%
                         unnest_tokens(word, text) %>%
                         anti_join(bind_rows(data.frame(word = stop_words$word),
                                             data.frame(word = stopwords::stopwords(source = 'smart')),
@@ -152,11 +121,41 @@ comments_tokenized <- comments_recent3  %>%
 
 
 comments_tokenized <- comments_tokenized %>%
-  left_join(select(comments_recent3, by, id, parent, type, time, cnt_links, cnt_qout, cnt_exclam), by='id')
+  left_join(select(comments_recent, by, id, parent, type, time, cnt_links, cnt_qout, cnt_exclam), by='id')
 
 comments_tokenized <- comments_tokenized[!duplicated(comments_tokenized), ]
 
 comments_tokenized$token_cnt <- str_count(comments_tokenized$text, '\\w+')
+
+
+ABT_comment <- comments_tokenized %>% 
+  group_by(by, time) %>% 
+  summarize(sum_qout = sum(cnt_qout),
+            sum_exclam = sum(cnt_exclam),
+            sum_link = sum(cnt_links),
+            cnt_comment = n(),
+            avg_token_len = mean(token_cnt))
+
+str(ABT_comment)
+
+saveRDS(ABT_comment, 'G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_month_user_agg.rds')
+
+
+
+
+"""
+comments_recent1 <- comments_recent[1:1000000, ]
+comments_recent2 <- comments_recent[1000001:2000000, ]
+comments_recent3 <- comments_recent[2000001:2824962, ]
+
+saveRDS(comments_recent1, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent1.rds')
+saveRDS(comments_recent2, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent2.rds')
+saveRDS(comments_recent3, file='G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent3.rds')
+
+
+comments_recent1 <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent1.rds')
+comments_recent2 <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent2.rds')
+comments_recent3 <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comments_recent3.rds')
 
 
 #comment_tokenized1 <- comments_tokenized
@@ -181,21 +180,9 @@ comment_tokenized3 <- readRDS('G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hacka
 
 df <- rbind(comment_tokenized1, comment_tokenized2, comment_tokenized3)
 
-ABT_comment <- df %>% 
-  group_by(by, time) %>% 
-  summarize(sum_qout = sum(cnt_qout),
-            sum_exclam = sum(cnt_exclam),
-            sum_link = sum(cnt_links),
-            cnt_comment = n(),
-            avg_token_len = mean(token_cnt))
-
-str(ABT_comment)
-
-saveRDS(ABT_comment, 'G:/Saját meghajtó/HiFly/Common/Homokozó/nlp-hackathon/data/transformed/comment_month_user_agg.rds')
 
 
-ggplot(ABT_comment, aes(x = sum_exclam)) + geom_histogram()
 
-min(ABT_comment$avg_token_len)
+"""
 
 
